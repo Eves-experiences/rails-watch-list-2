2a1ed02445bf77a11b08b50c9e8b636f30cb9a26
Evelien Lekkerkerk <e.lekkerkerk@gmail.com>
2023-01-19 15:10:16 +0100

    cleaned database migrations

--- db/migrate/20230119134000_add_list_id_to_movies.rb
+++
@@ -1,5 +0,0 @@
-class AddListIdToMovies < ActiveRecord::Migration[7.0]
-  def change
-    add_column :movies, :list_id, :integer
-  end
-end
--- db/migrate/20230119134213_add_movie_id_to_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddMovieIdToLists < ActiveRecord::Migration[7.0]
-  def change
-    add_column :lists, :movie_id, :integer
-  end
-end
