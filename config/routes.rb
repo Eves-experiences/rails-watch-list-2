961e55c811bd0660b6ffa141ecf74024ce927cab
Evelien Lekkerkerk <e.lekkerkerk@gmail.com>
2023-01-23 09:09:49 +0100

    rspec complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :movies
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
